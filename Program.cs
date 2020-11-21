9a895a5c8a11c65887df0c60e5a9a96fdae43ddb
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-21 19:39:42 +0530

    ADDED:distance between two points to calculate the distance between two points

--- Program.cs
+++ Program.cs
@@ -6,9 +6,33 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparasion Computation!");
+            double xCoordinates = GetEndPointsOfX();
+            double yCoordinates = GetEndPointsOfY();
+            double distance = DistanceBwtTwoPoints(xCoordinates, yCoordinates);
+            Console.WriteLine("Distance between the two points::" + distance);
             Console.ReadKey();
         }
 
+        public static double GetEndPointsOfX()
+        {
+            Console.WriteLine("Enter X1,X2");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            return Math.Pow((x2 - x1), 2);
+        }
+
+        public static double GetEndPointsOfY()
+        {
+            Console.WriteLine("Enter Y1,Y2");
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+            return Math.Pow((y2 - y1), 2);
+        }
+        
+        public static double DistanceBwtTwoPoints(double xCordinates,double yCoridinates)
+        {
+            double distance=Math.Sqrt(xCordinates + yCoridinates);
+            return Math.Round(distance, 2);
+        }
     }
 }
